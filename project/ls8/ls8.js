535a4bdc91d3bc42523c5d5451fbf3c80dced5f8
Christopher Beards <beardscm@gmail.com>
2018-04-10 18:52:31 -0400

    Step 7; Complete and Functional

--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -1,5 +1,6 @@
 const RAM = require('./ram');
 const CPU = require('./cpu');
+const fs = require('fs');
 
 /**
  * Load an LS8 program into memory
@@ -9,28 +10,16 @@ const CPU = require('./cpu');
 function loadMemory() {
 
     // Hardcoded program to print the number 8 on the console
-
-    const program = [ // print8.ls8
-        // "10011001", // LDI R0,8  Store 8 into R0
-        // "00000000",
-        // "00001000",
-        // "01000011", // PRN R0    Print the value in R0
-        // "00000000",
-        // "00000001"  // HLT       Halt and quit
-        "10011001", // # LDI R0,8
-        "00000000",
-        "00001000",
-        "10011001", // # LDI R1,9
-        "00000001",
-        "00001001",
-        "10101010", // # MUL R0,R1 <---
-        "00000000",
-        "00000001",
-        "01000011", // # PRN R0
-        "00000000",
-         "00000001" // # HLT
-       
-    ];
+    const file = process.argv[2];
+    const program = fs
+      .readFileSync(file)
+      .toString()
+      .split('\n')
+      .reduce((arr, elem) => {
+        if (elem[0] !== '#' && elem !== '') return arr.concat(elem.slice(0, 8));
+        else return arr;
+      }, []);
+    
 
     // Load the program into the CPU's memory a byte at a time
     for (let i = 0; i < program.length; i++) {
